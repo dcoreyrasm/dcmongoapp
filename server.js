ed04ef01fa8330127c110341a17f273e6f872c3d
dcoreyrasm <dcoreyrasm@users.noreply.github.com>
2017-01-10 20:08:11 -0500

    testing deployment
    testing deployment

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://dcoreyrasm2:rasmussen@ds161475.mlab.com:61475/rasmmongodb', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('rasmtables').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {rasmtables: result})
+  })
+})
+
+app.post('/', (req, res) => {
+  db.collection('rasmtables').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/', (req, res) => {
+  db.collection('rasmtables')
+  .findOneAndUpdate({name: 'Darice'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('An instructor quote got deleted')
+  })
+})
